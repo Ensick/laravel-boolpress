afca30004625baa39ca9bbf0699146e11deb9017
Ensick <fede.censi96@gmail.com>
2023-01-25 09:45:32 +0100

    fine esercizio

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            Work in progress
+        </h1>
+    </div>
+</template>
+
+<script>
+/* import { defineComponent } from '@vue/composition-api' */
+
+export default {
+    name:'App',
+    components: {
+
+    }
+}
+</script>
+
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Auth::routes();
+
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+
+    Route::get('/', 'HomeController@index')->name('index');
 });
+
+
+Route::get('{any?}', function () {
+
+    return view('guest.home');
+
+})->where('any','.*');
+
+
